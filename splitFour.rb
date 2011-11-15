97085a72c051ad4d26c0d66684d7f6f3c3dd8e90
depsir <depsir@gmail.com>
2011-11-15 23:43:01 +0100

    worked on pdf infos and ghostscript

--- splitFour.rb
+++ splitFour.rb
@@ -42,27 +42,20 @@ tmpFolder="/tmp/" + filename + rand(10000).to_s
 Dir.mkdir(tmpFolder)
 puts "Creata tmp folder: " + tmpFolder
 
-#pdftk $1 burst output $tmpFolder/%04d.pdf
-#echo "Estratte singole pagine"
-
-
-# split the input document into subpages
-
-# get page size
-a=5
-a+=1
-
 pages = %x[pdfinfo -f 1 -l 3 Cap3.pdf | grep "Page\ "]
 pages.each do |lol|
-#	lol.each_line('x') {|s| puts s}
+	pageNumber
 	width = Integer(lol.partition(/[0-9]+ x [0-9]+/)[1].partition(" x ")[0])
 	height = Integer(lol.partition(/[0-9]+ x [0-9]+/)[1].partition(" x ")[2])
 	puts "w #{width}"
 	puts "h #{height}"
-end
+	#%x{gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dFirstPage=#{pageNumber} -dLastPage=#{pageNumber} -sOutputFile="out.pdf" "#{filename}"}
+	#puts %x[gs -sDEVICE=pdfwrite -o outputPdf.pdf -c "[/CropBox [54 54 1314 810] /PAGES pdfmark" -f inputPdf.pdf]
+	#puts %x[gs -sDEVICE=pdfwrite -o outputPdf.pdf -c "[/CropBox [54 54 1314 810] /PAGES pdfmark" -f inputPdf.pdf]
+	#puts %x[gs -sDEVICE=pdfwrite -o outputPdf.pdf -c "[/CropBox [54 54 1314 810] /PAGES pdfmark" -f inputPdf.pdf]
+	#puts %x[gs -sDEVICE=pdfwrite -o outputPdf.pdf -c "[/CropBox [54 54 1314 810] /PAGES pdfmark" -f inputPdf.pdf]
 
-# crop and order subpages
-#puts %x[gs -sDEVICE=pdfwrite -o outputPdf.pdf -c "[/CropBox [54 54 1314 810] /PAGES pdfmark" -f inputPdf.pdf]
+end
 
 # merge subpages
 
